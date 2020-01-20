3afcf10d3567a456a038ad1ffbf006f054200e3a
wengy12 <wengy12>
2020-01-19 21:45:07 -0500

    test1

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import Customers from "./components/customers/customers";
 
 function App() {
+    let audio = new Audio("../server/output.mp3");
+
+    const start = () => {
+        audio.play()
+    }
+
   return (
+
+
     <div className="App">
+        <button onClick={start}>Play</button>
       <Customers></Customers>
     </div>
   );
