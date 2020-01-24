14696f60998e38aa7f15ef90dccfe88ee9e3804d
wengy12 <wengy12>
2020-01-24 03:04:45 +0300

    speech is up

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
+import Speech from 'react-speech';
 import Customers from "./components/customers/customers";
 
 function App() {
-    let audio = new Audio("../server/output.mp3");
 
-    const start = () => {
-        audio.play()
-    }
-
-  return (
-
-
-    <div className="App">
-        <button onClick={start}>Play</button>
-      <Customers></Customers>
-    </div>
-  );
+    return (
+        <div className = "App" >
+            <Customers > </Customers> 
+        </div >
+    );
 }
 
 export default App;
