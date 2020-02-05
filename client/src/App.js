3c3794a82c084529b1f15cb90f3049d3336c320e
wengy12 <wengy12>
2020-02-05 20:50:47 +0300

    good

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Speech from "react-speech";
 import Customers from "./components/customers/customers";
 
 function App() {
-  speech = text => {
-    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
-  };
-
   return (
     <div className="App">
       <Customers> </Customers>
-      <button text="Hello World" onClick={() => this.speech("Hello world")} />
     </div>
   );
 }
--- client/src/components/customers/customers.jsx
+++ client/src/components/customers/customers.jsx
@@ -3,33 +3,20 @@ import React, { Component } from "react";
 class Customers extends Component {
   constructor() {
     super();
-    this.state = { customers: [] };
+    
   }
 
-  componentDidMount() {
-    fetch("/api/customers")
-      .then(res => res.json())
-      .then(customers =>
-        this.setState({ customers }, () =>
-          console.log("Customers fetched..", customers)
-        )
-      );
+  speech = text => {
+    console.log("FUCK YOUFUDLISAFU");
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
   }
 
   render() {
     return (
       <React.Fragment>
-        <h3>Customers: Calling from Backend...</h3>
-        <ul>
-          {this.state.customers.map(customer => {
-            return (
-              <li key={customer.id}>
-                {customer.firstName} {customer.lastName}
-              </li>
-            );
-          })}
-        </ul>
+        <button onClick={() => this.speech("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")}>Something</button>
       </React.Fragment>
+      
     );
   }
 }
