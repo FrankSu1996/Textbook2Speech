f230dca67cb25dcd1ca1b0f0e24a58f0451e7486
wengy12 <wengy12>
2020-02-06 00:24:43 +0300

    speech

--- client/src/components/customers/customers.jsx
+++ client/src/components/customers/customers.jsx
@@ -3,18 +3,46 @@ import React, { Component } from "react";
 class Customers extends Component {
   constructor() {
     super();
-    
+    this.state = {
+      speechConfig: new SpeechSynthesisUtterance(),
+    }
   }
 
-  speech = text => {
+  speech = (text, u) => {
     console.log("FUCK YOUFUDLISAFU");
-    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+    u.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+    u.lang = 'en-US';
+    u.rate = 1;
+    speechSynthesis.speak(u);
+    u.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+    speechSynthesis.speak(u);
+    u.text = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+    speechSynthesis.speak(u);
+    u.text = "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    speechSynthesis.speak(u);
+  }
+
+  pause = u => {
+    speechSynthesis.pause();
+  }
+
+  cancel = u =>{
+    speechSynthesis.cancel();
+  }
+
+  resume = u =>{
+    u.rate = 1;
+    speechSynthesis.resume();
   }
 
   render() {
+    var u = new SpeechSynthesisUtterance();
     return (
       <React.Fragment>
-        <button onClick={() => this.speech("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")}>Something</button>
+        <button onClick={() => this.speech("", u)}>Something</button>
+        <button onClick={() => this.pause(u)}>pause</button>
+        <button onClick={() => this.resume(u)}>resume</button>
+        <button onClick={() => this.cancel(u)}>cancel</button>
       </React.Fragment>
       
     );
