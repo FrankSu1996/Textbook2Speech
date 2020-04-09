89879d8eb4b47232139f5b07cc2eaf2f19c4022b
Anna J <jos5@mcmaster.ca>
2020-04-09 16:12:08 -0400

    Added arrow images to tutorial

--- client/src/components/Tutorial.js
+++ client/src/components/Tutorial.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../index.css';
+import leftArrow from '../images/left_arrow.png';
+import upArrow from '../images/up_arrow.png';
+import downArrow from '../images/down_arrow.png';
+import rightArrow from '../images/right_arrow.png';
+
 
 class Tutorial extends React.Component {
   constructor(props){
@@ -34,7 +39,15 @@ class Tutorial extends React.Component {
         <div className='popup_inner'>
 
           <button onClick={this.closeTutorial}>X</button>
-          <p style={{fontSize: "20px"}}> {this.state.text} </p>
+          <p style={{fontSize: "20px"}}> With Textbook2Speech, you can navigate and interact with your textbooks using only your keyboard.
+           <br/>
+           <b>Keyboard commands</b>
+           <br/>
+           Press s to start the textbook reader. Press the <img src={upArrow} />  and <img src={downArrow} /> arrow keys to change the navigation state.
+           Press the <img src={leftArrow} /> and <img src={rightArrow} /> arrow keys to navigate through the current navigation state.
+           Press t to access this tutorial at any time.
+          </p>
+
 
         </div>
       </div>
